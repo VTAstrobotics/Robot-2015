7048675cebbab21393ae012340569b8c6ba0f970
Anirudh Bagde <anidev.aelico@gmail.com>
2015-04-23 12:28:43 -0400

    Improved formatting

--- RobotMain.ino
+++ RobotMain.ino
@@ -28,50 +28,47 @@ void printData(ControlData& data) {
 
 void motorControl(ControlData& data) {
     // Update state
-    if(data.id == DRIVE_LEFT || data.id == DRIVE_RIGHT) //drivetrain
-    {
-    if(data.id == DRIVE_LEFT) 
-    {
-        speedL = data.val;
-    } 
-    else if(data.id == DRIVE_RIGHT) {
-        speedR = data.val;
-    }
-    // Send state to drive slave
-    byte speed[] = { speedL, speedL, speedR, speedR };
-    Wire.beginTransmission(ADDR_DRIVE_SLAVE);
-    Wire.write(speed, 4);
-    Wire.endTransmission();
-    } 
-    
-    else if(data.id == DIG || data.id == DUMP || data.id == ACTUATOR_UP || data.id == ACTUATOR_DOWN) //Drum and Arm
-   { 
-    if(data.id == DIG) { // Drum control
-        // Assuming this one is 90-180 and dump is 90-0
-        speedDrum = data.val;
-    } else if(data.id == DUMP) {
-        speedDrum = -data.val + 180;
-    } else if(data.id == ACTUATOR_UP || data.id == ACTUATOR_DOWN) { // Actuator control
-        if(data.val == 0) { // Stopped pressing button
-            speedActuator1 = 90;
-            speedActuator2 = 90;
-        } else if(data.id == ACTUATOR_UP) {
-            speedActuator1 = SPEED_ACTUATOR1_UP;
-            speedActuator2 = SPEED_ACTUATOR2_UP;
-        } else if(data.id == ACTUATOR_DOWN) {
-            speedActuator1 = SPEED_ACTUATOR1_DOWN;
-            speedActuator2 = SPEED_ACTUATOR2_DOWN;
+    if(data.id == DRIVE_LEFT || data.id == DRIVE_RIGHT) { //drivetrain
+        if(data.id == DRIVE_LEFT) {
+            speedL = data.val;
+        } else if(data.id == DRIVE_RIGHT) {
+            speedR = data.val;
         }
+        // Send state to drive slave
+        byte speed[] = { speedL, speedL, speedR, speedR };
+        Wire.beginTransmission(ADDR_DRIVE_SLAVE);
+        Wire.write(speed, 4);
+        Wire.endTransmission();
     }
-    byte drum[] = {0, speedDrum, speedActuator1, speedActuator2};
-    Wire.beginTransmission(ADDR_DRUM_SLAVE);
-    Wire.write(drum, 4);
-    Wire.endTransmission();
-   }
-   
+
+    else if(data.id == DIG || data.id == DUMP || data.id == ACTUATOR_UP
+            || data.id == ACTUATOR_DOWN) { //Drum and Arm
+        if(data.id == DIG) { // Drum control
+            // Assuming this one is 90-180 and dump is 90-0
+            speedDrum = data.val;
+        } else if(data.id == DUMP) {
+            speedDrum = -data.val + 180;
+        } else if(data.id == ACTUATOR_UP || data.id == ACTUATOR_DOWN) { // Actuator control
+            if(data.val == 0) { // Stopped pressing button
+                speedActuator1 = 90;
+                speedActuator2 = 90;
+            } else if(data.id == ACTUATOR_UP) {
+                speedActuator1 = SPEED_ACTUATOR1_UP;
+                speedActuator2 = SPEED_ACTUATOR2_UP;
+            } else if(data.id == ACTUATOR_DOWN) {
+                speedActuator1 = SPEED_ACTUATOR1_DOWN;
+                speedActuator2 = SPEED_ACTUATOR2_DOWN;
+            }
+        }
+        byte drum[] = { 0, speedDrum, speedActuator1, speedActuator2 };
+        Wire.beginTransmission(ADDR_DRUM_SLAVE);
+        Wire.write(drum, 4);
+        Wire.endTransmission();
+    }
 
     char out[64];
-    sprintf(out, "Drive left: %d, right: %d, Drum speed: %d", speedL, speedR, speedDrum);
+    sprintf(out, "Drive left: %d, right: %d, Drum speed: %d", speedL, speedR,
+            speedDrum);
     Serial.println(out);
 }
 
