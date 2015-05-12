60a42018e5a1fd85fa4ec1337c5e6b98f6e2a634
Anirudh Bagde <anidev.aelico@gmail.com>
2015-05-12 19:34:41 -0400

    Added skeleton for ping test code

--- NetComm.cpp
+++ NetComm.cpp
@@ -7,6 +7,7 @@
 
 #include "NetComm.h"
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
@@ -14,7 +15,8 @@
 #include <sys/ioctl.h>
 #include "crc-16.h"
 
-NetComm::NetComm() {
+NetComm::NetComm() :
+        pingSent(false), pingTime(0.0) {
     recvSock = socket(AF_INET, SOCK_DGRAM, 0);
     int reuse = 1;
     setsockopt(recvSock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
@@ -22,7 +24,7 @@ NetComm::NetComm() {
     bzero(&addr, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(NETCOMM_RECVPORT);
+    addr.sin_port = htons(RECVPORT);
     bind(recvSock, (sockaddr*) &addr, sizeof(addr));
     fcntl(recvSock, F_SETFL, O_NONBLOCK);
     bzero(&ifr, sizeof(ifr));
@@ -39,7 +41,7 @@ bool NetComm::getData(ControlData* data) {
     bzero(data, sizeof(ControlData));
     bzero(&rawData, size);
     int len = recvfrom(recvSock, &rawData, size, 0, NULL, NULL);
-    if (len < size) {
+    if(len < size) {
         return false;
     }
     unsigned short crc16Check = crc16((unsigned char*) &rawData,
@@ -50,11 +52,11 @@ bool NetComm::getData(ControlData* data) {
     data->id = rawData.id;
     data->val = rawData.val;
     unsigned int dpad = rawData.val;
-    if(dpad & (1<<6)) {
+    if(dpad & (1 << 6)) {
         data->dpadY = 1;
-    } else if(dpad & (1<<4)) {
+    } else if(dpad & (1 << 4)) {
         data->dpadY = -1;
-    } else if(dpad & (1<<2)) {
+    } else if(dpad & (1 << 2)) {
         data->dpadX = 1;
     } else if(dpad & 1) {
         data->dpadX = -1;
@@ -63,9 +65,26 @@ bool NetComm::getData(ControlData* data) {
 }
 
 bool NetComm::isNetworkUp() {
+    // Make sure interface is up
     if(ioctl(recvSock, SIOCGIFFLAGS, &ifr) != -1) {
         int check_flags = IFF_UP | IFF_RUNNING;
-        return (ifr.ifr_flags & check_flags) == check_flags;
+        if((ifr.ifr_flags & check_flags) != check_flags) {
+            return false;
+        }
     }
-    return false;
+    // Ping the driver station every so often
+    double elapsed = getTime() - pingTime; // time since last ping was sent
+    if(pingSent) {
+        return elapsed < PING_WAIT; // return true until it times out
+    } else if(elapsed > PING_INTERVAL) {
+        // TODO send ping
+    }
+    return true; // assume it's fine for now
+}
+
+double NetComm::getTime() {
+    timespec time;
+    bzero(&time, sizeof(time));
+    clock_gettime(CLOCK_MONOTONIC, &time);
+    return time.tv_sec + (time.tv_nsec / 1.0e9);
 }
--- NetComm.h
+++ NetComm.h
@@ -11,51 +11,26 @@
 #include <stdint.h>
 #include <net/if.h>
 
-const int NETCOMM_RECVPORT = 6800;
-
 // Little-endian on Galileo (x86)
-/*struct __attribute__((__packed__)) Joystick {
-    float thumbLX;
-    float thumbLY;
-    float thumbRX;
-    float thumbRY;
-    float triggerL;
-    float triggerR;
-    unsigned int btnA :1;
-    unsigned int btnB :1;
-    unsigned int btnX :1;
-    unsigned int btnY :1;
-    unsigned int btnLB :1;
-    unsigned int btnRB :1;
-    unsigned int btnBack :1;
-    unsigned int btnStart :1;
-    unsigned int btnThumbL :1;
-    unsigned int btnThumbR :1;
-    unsigned int btnXbox :1;
-    signed int dpadX :2;
-    signed int dpadY :2;
-};
-
-struct __attribute__((__packed__)) ControlData {
-    Joystick joy1;
-    unsigned short crc16;
-};*/
-
 struct __attribute__((__packed__)) CommData {
-    uint8_t  id;
-    uint8_t  val;
+    uint8_t id;
+    uint8_t val;
     uint16_t crc16;
 };
 
 struct ControlData {
     uint8_t id;
     uint8_t val;
-    int8_t  dpadY;
-    int8_t  dpadX;
+    int8_t dpadY;
+    int8_t dpadX;
 };
 
 class NetComm {
 public:
+    static const int RECVPORT = 6800;
+    static const int PING_INTERVAL = 3000;
+    static const int PING_WAIT = 250;
+
     NetComm();
     ~NetComm();
 
@@ -63,8 +38,13 @@ public:
     bool isNetworkUp();
 
 private:
+    static double getTime();
+
     int recvSock;
     ifreq ifr; // For detecting if network interface is up
+    bool pingSent;
+    double pingTime;
+
 };
 
 #endif /* ROBOT_2015_NETCOMM_H_ */
